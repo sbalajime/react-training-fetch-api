3e3a112fd1eb91fe334225e5112c5cfc1afe26eb
Balaji Shanmugavel <sbalaji@protonmail.com>
2021-06-08 08:55:19 +0530

    session-6-7th-june

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      temperature: ''
+    }
+  }
+
+
+
+  delayData = () => {
+    return new Promise((resolve, reject) => {
+
+      setTimeout(()=>{ resolve(3)}, 2000)
+
+    })
+    
+  }
+
+  handleClick = () => {
+    console.log('1');
+    console.log('2');
+    // this.delayData()
+    // .then(data => data + 3)
+    // .then(data => data + 2)
+    // .then(data => console.log('data', data))
+    // .catch(err => console.log(err))
+    fetch('https://goweather.herokuapp.com/weather/thoothukudi')
+    .then(res => res.json())
+    .then(data => this.setState({temperature: data.temperature}))
+    console.log('4');
+    console.log('5');
+  }
+  render(){
+    return(<div>
+      <div>{this.state.temperature}</div>
+      <button onClick={this.handleClick}>Submit</button>
+      </div>)
+  }
 }
 
 export default App;
