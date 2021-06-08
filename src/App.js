d776b2e9770f97221991f6d973e370ef6205f55d
Balaji Shanmugavel <sbalaji@protonmail.com>
2021-06-08 16:33:29 +0530

    fetch call

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
     
   }
 
-  handleClick = () => {
+  handleClick = async () => {
     console.log('1');
     console.log('2');
     // this.delayData()
@@ -27,17 +27,41 @@ class App extends Component {
     // .then(data => data + 2)
     // .then(data => console.log('data', data))
     // .catch(err => console.log(err))
+    const data = await fetch('https://goweather.herokuapp.com/weather/thoothukudi')
+    const dataObj = await data.json()
+    console.log('data', dataObj)
+    console.log('4');
+    console.log('5');
+
     fetch('https://goweather.herokuapp.com/weather/thoothukudi')
     .then(res => res.json())
-    .then(data => this.setState({temperature: data.temperature}))
+    .then(res => {
+      console.log('data', res);
+   
+    })
     console.log('4');
     console.log('5');
   }
+
+
+  onClick = () => {
+    this.handleClick();
+    console.log('6');
+    console.log('7');
+  }
+
   render(){
     return(<div>
-      <div>{this.state.temperature}</div>
-      <button onClick={this.handleClick}>Submit</button>
+      <Temperature temperature={this.state.temperature}/>
+      <button onClick={this.onClick}>Submit</button>
       </div>)
+  }
+}
+
+class Temperature extends Component {
+  render(){
+    console.log('state', this.state, 'props')
+    return <div>{this.props.temperature}</div>
   }
 }
 
